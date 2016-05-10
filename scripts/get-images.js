be3af506fad9ba7e40c93843d4c07a62ebe4d8b9
Gerard Ryan <gerard@ryan.lt>
2016-05-10 09:46:05 +0100

    Add npm script to output image tags & command
    Motivation:
    It's a pain to get the details of the images from a template on a
    cluster during development or verification, so that they can be pulled
    on the Openshift nodes.
    
    Modification:
    Added a script that can be run from the root of the templates directory
    that will output a list of the images specified in the template, and
    also the command that can be used to pull them all with Docker.
    
    Result:
    It is now possible to run `npm run get-images` to get all of the images
    defined for an MBaaS, rather than the old way of looking into the
    template and manually concatenating the two relevant parameters for each
    image and constructing the docker pull commands from those.
    
    Currently the path to the single-node MBaaS template is hardcoded, this
    can be addressed in future if necessary.

---
+++ scripts/get-images.js
@@ -0,0 +1,40 @@
+var _ = require("lodash");
+var template = require("../fh-mbaas-template-1node.json");
+
+var NAME_SUFFIX_REGEXP = /_IMAGE$|_IMAGE_VERSION$/;
+
+function namePropertyWithoutSuffix(parameter) {
+  return _.replace(parameter.name, NAME_SUFFIX_REGEXP, "");
+}
+
+function reduceGroupedParams(paramGroup, accum) {
+  return _.reduce(paramGroup, function(result, elem) {
+    return result === accum ? result + elem.value : result + ":" + elem.value;
+  }, accum);
+}
+
+var relevantParameters = _.filter(template.parameters, function(parameter) {
+  return parameter.name &&
+    _.split(parameter.name, NAME_SUFFIX_REGEXP).length == 2;
+});
+
+var groupedComponents = _.groupBy(relevantParameters, namePropertyWithoutSuffix);
+
+var images = _.map(_.values(groupedComponents), function(group) {
+  return reduceGroupedParams(group, "");
+});
+
+var pullCommand = _.join(_.map(_.values(groupedComponents), function(group) {
+  return reduceGroupedParams(group, "sudo docker pull ");
+}), " && \\\n");
+
+console.log("Docker image tags defined in parameters:");
+console.log("---------------------------------------\n");
+
+_.forEach(images, function(image) {
+  console.log(image);
+});
+
+console.log("\nCommand to pull all images:");
+console.log("---------------------------");
+console.log(pullCommand);
