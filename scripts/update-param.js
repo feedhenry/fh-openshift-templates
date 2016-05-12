f5d458c9f480c1f11b7524fc67d9ebe8238603c0
Gerard Ryan <gerard@ryan.lt>
2016-05-12 23:46:18 +0100

    Add a version script for npm
    Motivation:
    When we change a template parameter automatically (e.g. a component
    version) in Jenkins, we want the ability to be able to also bump the
    package/template versions consistently also.
    
    Modification:
    Add a script, `./scripts/version.js`, which sets the version of in the
    template annotation to be whatever the value of the version in the
    `package.json` is. Also, set the `version` script in `package.json` to
    run this new script and add any modified files to the index (currently
    done outside the javascripts for convenience).
    
    Result:
    Running `npm version patch` will patch bump the version in the
    `package.json` and the two existing templates. It will also commit these
    changes and tag with the version.

--- scripts/update-param.js
+++ scripts/update-param.js
@@ -1,41 +1,6 @@
 var _ = require("lodash");
 var fs = require("fs");
-
-function getTemplates(files) {
-  return _.map(files, function(file) {
-    try {
-      fs.statSync(file);
-    } catch (e) {
-      console.error("ERROR:", e);
-      return {};
-    }
-
-    if (!fs.statSync(file).isFile()) {
-      console.warn(file, "is not a file");
-      return {};
-    }
-    return { name: file, contents: require(file) };
-  });
-}
-
-function writeTemplates(templates) {
-  _.forEach(templates, function(template) {
-    fs.writeFileSync(template.name, JSON.stringify(template.contents, null, 2));
-  });
-}
-
-function findAndUpdateParameter(templates, paramName, newValue) {
-  _.forEach(templates, function(template) {
-    var param = _.find(template.contents.parameters, function(candidate) {
-      return candidate.name && candidate.name == paramName;
-    });
-    if (param) {
-      param.value = newValue;
-    } else {
-      console.warn(paramName, "not found in", template.name);
-    }
-  });
-}
+var templates = require("../lib/templates.js");
 
 function main() {
   var argv = require('yargs')
@@ -46,9 +11,9 @@ function main() {
   var mbaasMulti = fs.realpathSync("fh-mbaas-template-3node.json");
   var mbaasSingle = fs.realpathSync("fh-mbaas-template-1node.json");
 
-  var templates = getTemplates([mbaasSingle, mbaasMulti]);
-  findAndUpdateParameter(templates, argv.p, argv.v);
-  writeTemplates(templates);
+  var templateMap = templates.get([mbaasSingle, mbaasMulti]);
+  templates.updateParameter(templateMap, argv.p, argv.v);
+  templates.write(templateMap);
 }
 
 main();
---
+++ scripts/version.js
@@ -0,0 +1,18 @@
+var fs = require("fs");
+var _ = require("lodash");
+var templates = require("../lib/templates.js");
+
+var version = require(fs.realpathSync("package.json")).version;
+var mbaasMulti = fs.realpathSync("fh-mbaas-template-3node.json");
+var mbaasSingle = fs.realpathSync("fh-mbaas-template-1node.json");
+
+var templateMap = templates.get([mbaasMulti, mbaasSingle]);
+_.forEach(templateMap, function(template) {
+  template.contents.metadata.annotations.templateVersion = version;
+});
+
+templates.write(templateMap);
+
+// _.forEach(templateMap, function(template) {
+
+// });
