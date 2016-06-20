a0ebeac6082c77d467d8c0b02780c1507936b335
Rodolfo Carvalho <rhcarvalho@gmail.com>
2016-06-20 17:11:43 +0200

    Remove useless flag added by kube package
    The flag is added globally by k8s.io/kubernetes/pkg/util/logs.go.

--- tools/template/main.go
+++ tools/template/main.go
@@ -5,9 +5,13 @@ import (
 
 	"github.com/fheng/fh-openshift-templates/tools/template/cmd"
 	_ "github.com/openshift/origin/pkg/api/install"
+	"github.com/spf13/pflag"
 )
 
 func main() {
+	// Hide flag automatically added by Kubernetes.
+	pflag.Lookup("log-flush-frequency").Hidden = true
+
 	if err := cmd.NewRootCommand(os.Stdin, os.Stdout, os.Stderr).Execute(); err != nil {
 		os.Exit(1)
 	}
