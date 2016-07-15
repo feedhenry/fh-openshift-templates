da3411ba93b167fbf6e7ca507c5a62425d0640a9
Gerard Ryan <gerard@ryan.lt>
2016-07-15 11:06:34 +0100

    Update automation script to include all templates
    Motivation:
    Some templates were missing from this, so haven't been getting automatic
    update PRs.
    
    Modification:
    * Added the missing templates
    * Sorted the list in the script
    * Fixed the name of the mysql* variables in the script
    * Ran the script, which updated some formatting in some templates (this
      would otherwise come in the next automated PR, so it makes sense to do
      it here)
    
    Result:
    The script should now be up-to-date. It should be updated with any new
    or removed, or moved templates as changes occur, until we decide that we
    want it to look for all template files (probably requires some thought).

--- scripts/update-param.js
+++ scripts/update-param.js
@@ -11,31 +11,43 @@ function main() {
   var mbaasMulti = fs.realpathSync("fh-mbaas-template-3node.json");
   var mbaasSingle = fs.realpathSync("fh-mbaas-template-1node.json");
 
+  var fhAppstoreSingle = fs.realpathSync("fh-appstore-single-template.json");
+  var fhAAASingle = fs.realpathSync("fh-aaa-single-template.json");
+  var fhMessagingSingle = fs.realpathSync("fh-messaging-single-template.json");
+  var fhMetricsSingle = fs.realpathSync("fh-metrics-single-template.json");
+  var fhNguiSingle = fs.realpathSync("fh-ngui-single-template.json");
   var fhScmSingle = fs.realpathSync("fh-scm-single-template.json");
   var fhSupercoreSingle = fs.realpathSync("fh-supercore-single-template.json");
-  var fhAAASingle = fs.realpathSync("fh-aaa-single-template.json");
+  var millicore = fs.realpathSync("fh-millicore-single-template.json");
+
   var gitlabShell = fs.realpathSync("gitlab-shell/gitlab-shell-single-template.json");
+  var httpd = fs.realpathSync("apache-httpd/httpd24-template.json");
   var memcached = fs.realpathSync("memcached/memcached-template.json");
   var mongodb = fs.realpathSync("mongodb/mongodb-core-single-template.json");
-  var millicore = fs.realpathSync("fh-millicore-single-template.json");
-  var httpd = fs.realpathSync("apache-httpd/httpd24-template.json");
-
-  var mysqlPesistent = fs.realpathSync("mysql/mysql-non-persistent-template.json");
-  var mysqlNonPesistent = fs.realpathSync("mysql/mysql-persistent-template.json");
+  var mysqlPersistent = fs.realpathSync("mysql/mysql-non-persistent-template.json");
+  var mysqlNonPersistent = fs.realpathSync("mysql/mysql-persistent-template.json");
+  var nagios = fs.realpathSync("nagios/nagios-template.json");
+  var redis = fs.realpathSync("redis/redis-template.json");
 
   var templateMap = templates.get([
-    mbaasSingle,
     mbaasMulti,
+    mbaasSingle,
+    fhAppstoreSingle,
+    fhAAASingle,
+    fhMessagingSingle,
+    fhMetricsSingle,
+    fhNguiSingle,
     fhScmSingle,
     fhSupercoreSingle,
-    fhAAASingle,
+    millicore,
     gitlabShell,
+    httpd,
     memcached,
     mongodb,
-    millicore,
-    httpd,
-    mysqlPesistent,
-    mysqlNonPesistent
+    mysqlPersistent,
+    mysqlNonPersistent,
+    nagios,
+    redis
   ]);
 
   templates.updateParameter(templateMap, argv.p, argv.v);
