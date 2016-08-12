204b5f023f4f17dfcf9ff09ccd3009cd89fc1656
Aiden Keating <akeating@redhat.com>
2016-08-12 16:25:04 +0100

    RHMAP-8189 Add ConfigMap to backend templates
    Currently the backend templates are sharing values using a script which
    imports the values. This isn't the best way to share values between templates.
    
    This adds ConfigMaps to various templates in the backend core template which
    allows for the values to be shared in a nicer way.

--- scripts/core/infra.sh
+++ scripts/core/infra.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-export SCRIPT_ROOT="$(dirname "${BASH_SOURCE}")"
-
-source $SCRIPT_ROOT/variables.sh
-
 #set up mysql mongo memcached
 
 oc new-app -f "${TEMPLATES_DIR}/generated/fh-core-infra.json"
