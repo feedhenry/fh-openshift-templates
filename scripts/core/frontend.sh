403179a254f71fffa4d75cea487382765bac3418
Aiden Keating <akeating@redhat.com>
2016-08-16 12:53:40 +0100

    RHMAP-8189 Remove unwanted parameter defaults
    Currently the parameters `CLUSTER_HOST` and `CLUSTER_DOMAIN` have
    default values which are unwanted.
    
    This updates the default values for these to be empty. The script for the
    frontend template has also been updated in order to provide these values.
    
    As the parameters are required the template will not be built unless a value
    for these parameters is provided.

--- scripts/core/frontend.sh
+++ scripts/core/frontend.sh
@@ -2,4 +2,4 @@
 
 # appstore, millicore, apache, ngui
 
-oc new-app -f ../../generated/fh-core-frontend.json --param=FH_ADMIN_USER_PASSWORD="Password1",GIT_EXTERNAL_PROTOCOL="http",CLUSTER_HOST="rhmap.local.feedhenry.io"
+oc new-app -f ../../generated/fh-core-frontend.json --param=FH_ADMIN_USER_PASSWORD="Password1",GIT_EXTERNAL_PROTOCOL="http",CLUSTER_HOST="rhmap.local.feedhenry.io",CLUSTER_DOMAIN="local.feedhenry.io"
