91a1ac151c1a1367fa5e9056231fa08512b08557
Gerard Ryan <gerard@ryan.lt>
2016-10-13 14:19:05 +0100

    Remove core templates from script and add new one

--- scripts/update-param.js
+++ scripts/update-param.js
@@ -10,42 +10,12 @@ function main() {
 
   var mbaasMulti = fs.realpathSync("fh-mbaas-template-3node.json");
   var mbaasSingle = fs.realpathSync("fh-mbaas-template-1node.json");
-
-  var fhAppstoreSingle = fs.realpathSync("fh-appstore-single-template.json");
-  var fhAAASingle = fs.realpathSync("fh-aaa-single-template.json");
-  var fhMessagingSingle = fs.realpathSync("fh-messaging-single-template.json");
-  var fhMetricsSingle = fs.realpathSync("fh-metrics-single-template.json");
-  var fhNguiSingle = fs.realpathSync("fh-ngui-single-template.json");
-  var fhScmSingle = fs.realpathSync("fh-scm-single-template.json");
-  var fhSupercoreSingle = fs.realpathSync("fh-supercore-single-template.json");
-  var millicore = fs.realpathSync("fh-millicore-single-template.json");
-  var ups = fs.realpathSync("unifiedpush-single-template.json");
-
-  var gitlabShell = fs.realpathSync("gitlab-shell/gitlab-shell-single-template.json");
-  var memcached = fs.realpathSync("memcached/memcached-template.json");
-  var mongodb = fs.realpathSync("mongodb/mongodb-core-single-template.json");
-  var mysqlPersistent = fs.realpathSync("mysql/mysql-persistent-template.json");
-  var nagios = fs.realpathSync("nagios/nagios-template.json");
-  var redis = fs.realpathSync("redis/redis-template.json");
+  var mbaasSinglePersistent = fs.realpathSync("fh-mbaas-template-1node-persistent.json");
 
   var templateMap = templates.get([
     mbaasMulti,
     mbaasSingle,
-    fhAppstoreSingle,
-    fhAAASingle,
-    fhMessagingSingle,
-    fhMetricsSingle,
-    fhNguiSingle,
-    fhScmSingle,
-    fhSupercoreSingle,
-    millicore,
-    gitlabShell,
-    memcached,
-    mongodb,
-    mysqlPersistent,
-    nagios,
-    redis,
-    ups
+    mbaasSinglePersistent
   ]);
 
   templates.updateParameter(templateMap, argv.p, argv.v);
