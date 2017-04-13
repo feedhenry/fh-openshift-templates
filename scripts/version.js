79817ebf4af52ad167be0864a60d5c95ce5bd414
Gerard Ryan <gerard@ryan.lt>
2017-04-13 09:43:29 +0100

    🚑 Fix scripts for changed template name

--- scripts/version.js
+++ scripts/version.js
@@ -5,9 +5,9 @@ var templates = require("../lib/templates.js");
 var version = require(fs.realpathSync("package.json")).version;
 var mbaasMulti = fs.realpathSync("fh-mbaas-template-3node.json");
 var mbaasSingle = fs.realpathSync("fh-mbaas-template-1node.json");
-var mbaasSinglePersistent = fs.realpathSync("fh-mbaas-template-1node-persistent.json");
+var mbaasSingleNonPersistent = fs.realpathSync("fh-mbaas-template-1node-non-persistent.json");
 
-var templateMap = templates.get([mbaasMulti, mbaasSingle, mbaasSinglePersistent]);
+var templateMap = templates.get([mbaasMulti, mbaasSingle, mbaasSingleNonPersistent]);
 _.forEach(templateMap, function(template) {
   template.contents.metadata.annotations.templateVersion = version;
 });
